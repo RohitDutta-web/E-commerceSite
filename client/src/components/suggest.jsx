fc1adea750d7b99cb56e8d93cd4eba92793f657b
rohit <duttarohit754@gmail.com>
2024-12-28 00:39:20 +0530

    AI feature

--- client/src/components/suggest.jsx
+++ client/src/components/suggest.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
+
+
 
 
 export default function Suggest() {
+
+  const [question, setQuestion] = useState("")
+  const [answer, setAnswer] = useState("")
+ 
+  
+
+
   return (
     <>
-      <div className="w-full h-auto flex flex-col items-center justify-center mt-20  pt-10 ">
-        <textarea readOnly className="resize-none max-w-screen w-4/5 h-auto outline-none bg-zinc-200   text-black p-3" value={'fafdad'}></textarea>
-        <input type="text" placeholder="Ask here for anything about any product" className="w-2/3" />
-        <button>Cook</button>
+      <div className="w-auto h-auto flex flex-col items-center justify-center mt-20 pb-3 pt-10 shadow-xl m-4">
+        <textarea readOnly className="resize-auto max-w-screen w-4/5 h-auto outline-none bg-zinc-100   text-black p-3  " value={''} ></textarea>
+        <input type="text" placeholder="🛍️ Got product questions? 💬 Ask away!" className="w-2/3 outline rounded outline-zinc-200 mt-3 p-2" onChange={(e) => setQuestion(e.target.value)} value={question} />
+        <button className="border-2 p-2 rounded font-bold border-black hover:text-white pl-5 pr-5  hover:bg-zinc-900 mt-5">Ask</button>
+        <p className="text-zinc-400 mt-5">E.g . Best phones under 20k or differences between iPhone15 and iPhone16</p>
+        
       </div>
     </>
   )
