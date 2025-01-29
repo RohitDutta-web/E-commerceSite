fd38297762268c094445ea9a891ada3aa56dfb3f
rohit <duttarohit754@gmail.com>
2025-01-29 20:02:18 +0530

    order controller

---
+++ server/controllers/order.controller.js
@@ -0,0 +1,11 @@
+import Order from "../models/order.model";
+
+export const placeOrder = async (req, res) => { }
+
+
+export const getOrder = async (req, res) => { }
+
+export const updateOrderStatus = async (req, res) => { }
+
+export const cancelOrder = async (req, res) => { }
+
