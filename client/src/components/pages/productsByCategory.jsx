be69d873b9a27d03849a935324b6b4345e2b9da2
rohit <duttarohit754@gmail.com>
2025-02-24 20:42:19 +0530

    Category wise products

--- client/src/components/pages/productsByCategory.jsx
+++ client/src/components/pages/productsByCategory.jsx
@@ -1,9 +1,54 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Skeleton from '@mui/material/Skeleton';
+import { Link } from "react-router-dom";
+import ProductCard from "../productCard";
 
+import NavBar from "../navBar";
+import { toast } from "sonner";
+import axios from "axios";
 
 export default function ProductsByCategory() {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const { category } = useParams();
+
+  const productsByCategory = async () => {
+    try {
+      setLoading(true)
+      const res = await axios.get(`http://localhost:3000/api/product/${category}`)
+      setProducts(res.data.products)
+      setLoading(false)
+      return
+    }
+    catch (e) {
+      toast.error(e.response?.data?.message || "Failed to fetch products")
+    }
+  }
+
+  useEffect(() => { productsByCategory() }, [])
   return (
     <>
-      ProductsByCategory
+      <NavBar />
+      <div className="w-full max-w-screen flex justify-center font-2xl font-bold">
+        <p>Category : {category}</p>
+        </div>
+        <div className="flex flex-wrap gap-5 mt-20 ml-5 mr-5">
+          {
+            loading ? (<>
+              <Skeleton variant="rectangular" width={210} height={118} />
+              <Skeleton variant="rectangular" width={210} height={118} />
+              <Skeleton variant="rectangular" width={210} height={118} />
+            </>) : (products.map((product) => (
+              <Link to={`/${product._id}`}
+                state={{ product }}
+                key={product._id}>
+                <ProductCard {...product} />
+              </Link>
+            )))
+          }
+        </div>
+    
     </>
   )
 }
